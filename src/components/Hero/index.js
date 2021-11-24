486b2bacaa543bfb8eeebfb6231285592d72f575
khrokham <rokhamkhawaja.rk22@gmail.com>
2021-11-24 16:02:03 +0500

    changed heading

--- src/components/Hero/index.js
+++ src/components/Hero/index.js
@@ -14,7 +14,7 @@ const Hero = () => {
             <Navbar toggle={toggle}/>
             <HeroContent>
                 <HeroItems>
-                    <HeroH1>Greates Pizza Ever</HeroH1>
+                    <HeroH1>Greatest Pizza Ever</HeroH1>
                     <HeroP>Ready in 60 seconds</HeroP>
                     <HeroBtn>Place Order</HeroBtn>
                 </HeroItems>
